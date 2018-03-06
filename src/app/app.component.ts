09384d20b4947f065b72b32b0490184b1ea6e03a
codingblazer <sasachin04@gmail.com>
2018-03-06 17:24:44 +0530

    Request to api working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Http, Response} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  private apiUrl = 'https://api.github.com/users/codingblazer/repos';
+  data : any = {};
+
+  constructor(private http : Http){
+    console.log('fetching data starts');
+    this.getRepo();
+    this.getData();
+  }
+
+  getData(){
+    return this.http.get(this.apiUrl).map((res : Response)=> res.json());
+  }
+
+  getRepo(){
+    this.getData().subscribe(data=>{
+      console.log(data[0].name);
+      this.data = data;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
@@ -10,9 +13,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
