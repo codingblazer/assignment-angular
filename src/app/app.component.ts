735efe4cf172c1556dad95f5bdf7092c35fcb1fd
codingblazer <sasachin04@gmail.com>
2018-03-06 20:23:41 +0530

    The UI part of showing results done using table and ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,9 @@ import 'rxjs/add/operator/map';
 })
 export class AppComponent {
   title = 'app';
-
-  data : any = {};
-  username : string = 'codingblazer';
-  repo : string = 'assignment-angular';
+  repos = [];
+  username : string = '';
+  repo : string = '';
 
   constructor(private http : Http){
     console.log('fetching data starts');
@@ -21,7 +20,14 @@ export class AppComponent {
   addRepoData(){
     console.log('I am in'+this.username+'  '+this.repo);
     var apiUrl : string = 'https://api.github.com/repos/'+this.username+'/'+this.repo;
+
+    //reset fields
+    this.username = '';
+    this.repo = '';
+
+    //api call
     return this.http.get(apiUrl).map((res : Response)=> res.json()).subscribe(data=>{
+      this.repos.push(data);
       console.log(data);
     });
   }
